aa2bfd1870fa741eac3f392f463b1e284721953a
Manuel <manutepowa@gmail.com>
2023-10-04 18:19:24 +0200

    ..

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -53,7 +53,6 @@
 
   # Add stuff for your user as you see fit:
   programs.neovim.enable = true;
-  # home.packages = with pkgs; [ steam ];
 
   # Enable home-manager and git
   programs.home-manager.enable = true;
@@ -65,6 +64,11 @@
 
   # Nicely reload system units when changing configs
   systemd.user.startServices = "sd-switch";
+
+  home.sessionVariables = {
+  	EDITOR = "nvim";
+	TERMINAL = "kitty";
+  };
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   home.stateVersion = "23.05";
